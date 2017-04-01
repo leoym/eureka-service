f8d68d536ace226817b754ec84c349a82ffdf852
Leonardo <leoyasu@yahoo.com.br>
2017-04-01 11:56:51 -0300

    V2.0

--- src/main/java/br/com/lym/devops/EurekaClientApplication.java
+++ src/main/java/br/com/lym/devops/EurekaClientApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -31,5 +32,10 @@ class ServiceInstanceRestController {
     public List<ServiceInstance> serviceInstancesByApplicationName(
             @PathVariable String applicationName) {
         return this.discoveryClient.getInstances(applicationName);
+    }
+    
+    @RequestMapping("/greeting")
+    public String Home (@RequestParam(value="name", defaultValue="World") String name) {
+        return "Hello" + name.toString();
     }
 }
